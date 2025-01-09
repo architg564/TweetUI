50d9b4e272b814dc9716ebefce9796682574877f
Archit Gupta <architg564@gmail.com>
2025-01-09 11:28:34 +0530

    fixed bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
-        path: '/home',
+        path: '',
+        component: AppComponent
+    },
+    {
+        path: 'home',
         component: HomeComponent
     }
 ];
